9e2e30e40d4eccf41f6bfeef03dbb08b507856c7
sbx_user1051 <sbx_user1051@169.254.10.161>
2025-01-19 14:20:34 +0000

    Sun Jan 19 14:20:34 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,5 +3,20 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    
+    # Price table
+    prices = {
+        'A': 50,
+        'B': 30,
+        'C': 20,
+        'D': 15
+    }
+
+    # Special offers
+    offers = {
+        'A': (3, 130),
+        'B': (2, 45)    
+    }
+
+    # Checking for invalid input - if the input `skus` is not a string or contains invalid characters
+    if not isinstance(skus, str) or not all()
 
